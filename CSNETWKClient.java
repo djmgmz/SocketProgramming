7846decac5bb58013485bd12fca8fd6adaf014c4
Dominic Gomez <dominic_joel_gomez@dlsu.edu.ph>
2024-06-05 17:18:43 +0800

    Initial commit

---
+++ CSNETWKClient.java
@@ -0,0 +1,44 @@
+import java.net.*;
+import java.io.*;
+
+public class CSNETWKClient
+{
+	public static void main(String[] args)
+	{
+		String sServerAddress = args[0];
+		int nPort = Integer.parseInt(args[1]);
+		
+		try
+		{
+			Socket clientEndpoint = new Socket(sServerAddress, nPort);
+			
+			System.out.println("Client: Connected to server at" + clientEndpoint.getRemoteSocketAddress());
+			
+			DataOutputStream dosWriter = new DataOutputStream(clientEndpoint.getOutputStream());
+			dosWriter.writeUTF("Client: Hello from client" + clientEndpoint.getLocalSocketAddress());
+			
+			DataInputStream disReader = new DataInputStream(clientEndpoint.getInputStream());
+			System.out.println(disReader.readUTF());
+
+			FileOutputStream fos = new FileOutputStream("Received.txt");
+
+			byte[] buffer = new byte[4096];
+			int bytesRead;
+			while ((bytesRead = disReader.read(buffer)) != -1) {
+				fos.write(buffer, 0, bytesRead);
+			}
+
+			System.out.println("Client: File received and saved as Received.txt");
+
+			clientEndpoint.close();
+		}
+		catch (Exception e)
+		{
+			e.printStackTrace();
+		}
+		finally
+		{
+			System.out.println("Client: Connection is terminated.");
+		}
+	}
+}
---
+++ CSNETWKServer.java
@@ -0,0 +1,52 @@
+import java.net.*;
+import java.io.*;
+
+public class CSNETWKServer
+{
+	public static void main(String[] args)
+	{
+		int nPort = Integer.parseInt(args[0]);
+		System.out.println("Server: Listening on port " + args[0] + "...");
+		ServerSocket serverSocket;
+		Socket serverEndpoint;
+
+		try 
+		{
+			serverSocket = new ServerSocket(nPort);
+			serverEndpoint = serverSocket.accept();
+			
+			System.out.println("Server: New client connected: " + serverEndpoint.getRemoteSocketAddress());
+
+			DataInputStream disReader = new DataInputStream(serverEndpoint.getInputStream());
+			System.out.println(disReader.readUTF());
+			
+			DataOutputStream dosWriter = new DataOutputStream(serverEndpoint.getOutputStream());
+			dosWriter.writeUTF("Server: Hello World!");
+
+			File file = new File("Download.txt");
+			if (file.exists() && !file.isDirectory()) {
+				FileInputStream fis = new FileInputStream(file);
+				byte[] buffer = new byte[4096];
+				int bytesRead;
+				while ((bytesRead = fis.read(buffer)) != -1) {
+					dosWriter.write(buffer, 0, bytesRead);
+				}
+				fis.close();
+				System.out.println("Server: File sent successfully");
+			} else {
+				dosWriter.writeUTF("ERROR: File not found");
+				System.out.println("Server: File not found");
+			}
+
+			serverEndpoint.close();
+		}
+		catch (Exception e)
+		{
+			e.printStackTrace();
+		}
+		finally
+		{
+			System.out.println("Server: Connection is terminated.");
+		}
+	}
+}
